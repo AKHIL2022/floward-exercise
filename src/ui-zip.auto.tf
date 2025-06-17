8a952379cae789b37f2f911bc964642036248f9c
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-06-17 10:29:57 +0000

    Update package.json ()

--- src/ui-zip.auto.tf
+++ src/ui-zip.auto.tf
@@ -1,7 +1,7 @@
 # Module: 
 locals {
-  _commitHash = "15fcb27879285cd9a0a958e276ed508e4fdbfb23"
-  _commitDate = "2025-06-17T13:59:02+05:30"
+  _commitHash = "f8d79236aa74fa87483c8a000eecd4e45084f6b6"
+  _commitDate = "2025-06-17T15:59:05+05:30"
   _bucketName = "tf-test-1"
-  _objectName = "HCLCODE_Test//v20250617-083035.zip"
+  _objectName = "HCLCODE_Test//v20250617-102921.zip"
 }
