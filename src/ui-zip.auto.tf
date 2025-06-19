bfa061eabccf495692ea9e344e1f79f169fef21a
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-06-19 06:41:28 +0000

    Update Jenkinsfile ()

--- src/ui-zip.auto.tf
+++ src/ui-zip.auto.tf
@@ -1,7 +1,7 @@
 # Module: 
 locals {
-  _commitHash = "8a952379cae789b37f2f911bc964642036248f9c"
-  _commitDate = "2025-06-17T10:29:57+00:00"
+  _commitHash = "e8d3cfdda684eda374b32c77a188cf79a11dd1e0"
+  _commitDate = "2025-06-19T12:10:26+05:30"
   _bucketName = "tf-test-1"
-  _objectName = "HCLCODE_Test//v20250617-103056.zip"
+  _objectName = "HCLCODE_Test//v20250619-064044.zip"
 }
